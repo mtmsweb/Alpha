9e498f58059a842e4dd0e5bd53f1471a68f4e6d3
mtmsweb <mtmsweb@gmail.com>
2017-01-15 14:53:55 +0100

    Update Main.java

--- Alpha/src/com/mtm/alpha/Main.java
+++ Alpha/src/com/mtm/alpha/Main.java
@@ -1,5 +1,12 @@
 package com.mtm.alpha;
 
+/**
+ * © 2016 mTm Michał Macioła Development
+ * Contact at mtmsweb@gmail.com
+ * License Apache 2.0
+ * https://github.com/mtmsweb/Alpha
+ */
+
 public class Main {
 
     public static void main(String[] args) {
